b0ebbd7629ccd478d48c8326c47f2480f7ee4c57
99002521 <swarnimshekhar4897@gmail.com>
2020-10-27 14:50:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lntdemo1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lntdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText nameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.editTextTextPersonName2); //init
+    }
+
+    public void clickHandler(View view) {
+        String name = nameEditText.getText().toString();  //using
+        Toast.makeText(this, "Welcome to Android "+name, Toast.LENGTH_SHORT).show();
+    }
+}
